200bfa7dde3fe06e11b316b20ca137caa25e97ae
RodolfoLobos <stevenlovoz3@gmail.com>
2023-08-29 16:09:51 -0600

    Parcial de programación Computacional

---
+++ Programación computacional 1 Parcial/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Programación_computacional_1_Parcial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboArea = new System.Windows.Forms.ComboBox();
+            this.txtCantidadArea = new System.Windows.Forms.TextBox();
+            this.cboAarea = new System.Windows.Forms.ComboBox();
+            this.cboDeArea = new System.Windows.Forms.ComboBox();
+            this.lblRespuestaArea = new System.Windows.Forms.Label();
+            this.lblDeCantidadArea = new System.Windows.Forms.Label();
+            this.lblAarea = new System.Windows.Forms.Label();
+            this.lblDEArea = new System.Windows.Forms.Label();
+            this.lblTipoArea = new System.Windows.Forms.Label();
+            this.btnConvertirArea = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboArea
+            // 
+            this.cboArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboArea.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboArea.FormattingEnabled = true;
+            this.cboArea.Items.AddRange(new object[] {
+            "Area"});
+            this.cboArea.Location = new System.Drawing.Point(373, 67);
+            this.cboArea.Name = "cboArea";
+            this.cboArea.Size = new System.Drawing.Size(226, 35);
+            this.cboArea.TabIndex = 41;
+            // 
+            // txtCantidadArea
+            // 
+            this.txtCantidadArea.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidadArea.Location = new System.Drawing.Point(372, 208);
+            this.txtCantidadArea.Name = "txtCantidadArea";
+            this.txtCantidadArea.Size = new System.Drawing.Size(226, 35);
+            this.txtCantidadArea.TabIndex = 40;
+            // 
+            // cboAarea
+            // 
+            this.cboAarea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboAarea.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboAarea.FormattingEnabled = true;
+            this.cboAarea.Items.AddRange(new object[] {
+            "A: Pie Cuadrado",
+            "B: Vara Cuadrada",
+            "C: Yarda Cuadrada",
+            "D Metro Cuadrado ",
+            "E: Tareas",
+            "F: Manzana",
+            "G: Hectáreas"});
+            this.cboAarea.Location = new System.Drawing.Point(373, 163);
+            this.cboAarea.Name = "cboAarea";
+            this.cboAarea.Size = new System.Drawing.Size(226, 35);
+            this.cboAarea.TabIndex = 39;
+            // 
+            // cboDeArea
+            // 
+            this.cboDeArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDeArea.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboDeArea.FormattingEnabled = true;
+            this.cboDeArea.Items.AddRange(new object[] {
+            "A: Pie Cuadrado",
+            "B: Vara Cuadrada",
+            "C: Yarda Cuadrada",
+            "D Metro Cuadrado ",
+            "E: Tareas",
+            "F: Manzana",
+            "G: Hectáreas"});
+            this.cboDeArea.Location = new System.Drawing.Point(373, 110);
+            this.cboDeArea.Name = "cboDeArea";
+            this.cboDeArea.Size = new System.Drawing.Size(226, 35);
+            this.cboDeArea.TabIndex = 38;
+            // 
+            // lblRespuestaArea
+            // 
+            this.lblRespuestaArea.AutoSize = true;
+            this.lblRespuestaArea.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuestaArea.Location = new System.Drawing.Point(201, 270);
+            this.lblRespuestaArea.Name = "lblRespuestaArea";
+            this.lblRespuestaArea.Size = new System.Drawing.Size(215, 32);
+            this.lblRespuestaArea.TabIndex = 37;
+            this.lblRespuestaArea.Text = "RESPUESTA: ?";
+            // 
+            // lblDeCantidadArea
+            // 
+            this.lblDeCantidadArea.AutoSize = true;
+            this.lblDeCantidadArea.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeCantidadArea.Location = new System.Drawing.Point(201, 211);
+            this.lblDeCantidadArea.Name = "lblDeCantidadArea";
+            this.lblDeCantidadArea.Size = new System.Drawing.Size(165, 32);
+            this.lblDeCantidadArea.TabIndex = 36;
+            this.lblDeCantidadArea.Text = "CANTIDAD:";
+            // 
+            // lblAarea
+            // 
+            this.lblAarea.AutoSize = true;
+            this.lblAarea.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAarea.Location = new System.Drawing.Point(201, 166);
+            this.lblAarea.Name = "lblAarea";
+            this.lblAarea.Size = new System.Drawing.Size(42, 32);
+            this.lblAarea.TabIndex = 35;
+            this.lblAarea.Text = "A:";
+            // 
+            // lblDEArea
+            // 
+            this.lblDEArea.AutoSize = true;
+            this.lblDEArea.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDEArea.Location = new System.Drawing.Point(201, 113);
+            this.lblDEArea.Name = "lblDEArea";
+            this.lblDEArea.Size = new System.Drawing.Size(62, 32);
+            this.lblDEArea.TabIndex = 34;
+            this.lblDEArea.Text = "DE:";
+            // 
+            // lblTipoArea
+            // 
+            this.lblTipoArea.AutoSize = true;
+            this.lblTipoArea.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoArea.Location = new System.Drawing.Point(201, 70);
+            this.lblTipoArea.Name = "lblTipoArea";
+            this.lblTipoArea.Size = new System.Drawing.Size(88, 32);
+            this.lblTipoArea.TabIndex = 33;
+            this.lblTipoArea.Text = "TIPO:";
+            // 
+            // btnConvertirArea
+            // 
+            this.btnConvertirArea.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertirArea.Location = new System.Drawing.Point(334, 344);
+            this.btnConvertirArea.Name = "btnConvertirArea";
+            this.btnConvertirArea.Size = new System.Drawing.Size(240, 40);
+            this.btnConvertirArea.TabIndex = 32;
+            this.btnConvertirArea.Text = "CONVERTIR";
+            this.btnConvertirArea.UseVisualStyleBackColor = true;
+            this.btnConvertirArea.Click += new System.EventHandler(this.btnConvertirArea_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboArea);
+            this.Controls.Add(this.txtCantidadArea);
+            this.Controls.Add(this.cboAarea);
+            this.Controls.Add(this.cboDeArea);
+            this.Controls.Add(this.lblRespuestaArea);
+            this.Controls.Add(this.lblDeCantidadArea);
+            this.Controls.Add(this.lblAarea);
+            this.Controls.Add(this.lblDEArea);
+            this.Controls.Add(this.lblTipoArea);
+            this.Controls.Add(this.btnConvertirArea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboArea;
+        private System.Windows.Forms.TextBox txtCantidadArea;
+        private System.Windows.Forms.ComboBox cboAarea;
+        private System.Windows.Forms.ComboBox cboDeArea;
+        private System.Windows.Forms.Label lblRespuestaArea;
+        private System.Windows.Forms.Label lblDeCantidadArea;
+        private System.Windows.Forms.Label lblAarea;
+        private System.Windows.Forms.Label lblDEArea;
+        private System.Windows.Forms.Label lblTipoArea;
+        private System.Windows.Forms.Button btnConvertirArea;
+    }
+}
+
---
+++ Programación computacional 1 Parcial/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programación_computacional_1_Parcial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            String[,] Area =  {
+      {"Pie Cuadrado", "Vara Cuadrada", "Yarda Cuadrada", "Metro Cuadrado", "Tareas", "Manzana", "Hectáreas" },
+         };
+            InitializeComponent();
+        }
+
+        private void btnConvertirArea_Click(object sender, EventArgs e)
+        {
+            int DE, A, conversor;
+            double ctd, r;
+
+            DE = cboDeArea.SelectedIndex;
+            A = cboAarea.SelectedIndex;
+            conversor = cboAarea.SelectedIndex;
+
+            ctd = double.Parse(txtCantidadArea.Text);
+            //Pie Cuadrado, Vara Cuadrada, Yarda Cuadrada, Metro Cuadrado, Tareas, Manzana, Hectáreas, 
+            double[][] Areas = {
+                new double[] { 10.7639, 0.698896, 0.836127, 1 , 628.8, 6474.9702758, 10000},
+            };
+
+            r = Areas[cboArea.SelectedIndex][DE] / Areas[cboArea.SelectedIndex][A] * ctd;
+            lblRespuestaArea.Text = "Respuesta: " + Math.Round(r, 6);
+        }
+    }
+}
